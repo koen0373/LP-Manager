1665b38b6c12b22d43dd02f3e624051fd64bb7ee
Koen Verhagen <koen@MacBook-Pro-van-Koen.local>
2025-10-24 07:36:32 +0200

    Fix Uniswap V3 Pool ABI: remove excess indexed parameters from Mint, Burn, Collect events

--- src/abis/UniswapV3Pool.ts
+++ src/abis/UniswapV3Pool.ts
@@ -7,8 +7,8 @@ export const UNISWAP_V3_POOL_ABI = [
     inputs: [
       { name: 'sender', type: 'address', indexed: true },
       { name: 'owner', type: 'address', indexed: true },
-      { name: 'tickLower', type: 'int24', indexed: true },
-      { name: 'tickUpper', type: 'int24', indexed: true },
+      { name: 'tickLower', type: 'int24', indexed: false },
+      { name: 'tickUpper', type: 'int24', indexed: false },
       { name: 'amount', type: 'uint128', indexed: false },
       { name: 'amount0', type: 'uint256', indexed: false },
       { name: 'amount1', type: 'uint256', indexed: false }
@@ -19,8 +19,8 @@ export const UNISWAP_V3_POOL_ABI = [
     type: 'event',
     inputs: [
       { name: 'owner', type: 'address', indexed: true },
-      { name: 'tickLower', type: 'int24', indexed: true },
-      { name: 'tickUpper', type: 'int24', indexed: true },
+      { name: 'tickLower', type: 'int24', indexed: false },
+      { name: 'tickUpper', type: 'int24', indexed: false },
       { name: 'amount', type: 'uint128', indexed: false },
       { name: 'amount0', type: 'uint256', indexed: false },
       { name: 'amount1', type: 'uint256', indexed: false }
@@ -44,9 +44,9 @@ export const UNISWAP_V3_POOL_ABI = [
     type: 'event',
     inputs: [
       { name: 'owner', type: 'address', indexed: true },
-      { name: 'recipient', type: 'address', indexed: true },
-      { name: 'tickLower', type: 'int24', indexed: true },
-      { name: 'tickUpper', type: 'int24', indexed: true },
+      { name: 'recipient', type: 'address', indexed: false },
+      { name: 'tickLower', type: 'int24', indexed: false },
+      { name: 'tickUpper', type: 'int24', indexed: false },
       { name: 'amount0', type: 'uint128', indexed: false },
       { name: 'amount1', type: 'uint128', indexed: false }
     ]

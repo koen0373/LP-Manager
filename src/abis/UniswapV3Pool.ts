55fb46ca653cee283124489086aef0f3423a44bd
Koen Verhagen <koen@MacBook-Pro-van-Koen.local>
2025-10-24 07:50:49 +0200

    Fix Pool ABI: sender is NOT indexed in Mint event (data contains sender+amount+amount0+amount1)

--- src/abis/UniswapV3Pool.ts
+++ src/abis/UniswapV3Pool.ts
@@ -5,10 +5,10 @@ export const UNISWAP_V3_POOL_ABI = [
     name: 'Mint',
     type: 'event',
     inputs: [
-      { name: 'sender', type: 'address', indexed: true },
+      { name: 'sender', type: 'address', indexed: false },
       { name: 'owner', type: 'address', indexed: true },
-      { name: 'tickLower', type: 'int24', indexed: false },
-      { name: 'tickUpper', type: 'int24', indexed: false },
+      { name: 'tickLower', type: 'int24', indexed: true },
+      { name: 'tickUpper', type: 'int24', indexed: true },
       { name: 'amount', type: 'uint128', indexed: false },
       { name: 'amount0', type: 'uint256', indexed: false },
       { name: 'amount1', type: 'uint256', indexed: false }
@@ -19,8 +19,8 @@ export const UNISWAP_V3_POOL_ABI = [
     type: 'event',
     inputs: [
       { name: 'owner', type: 'address', indexed: true },
-      { name: 'tickLower', type: 'int24', indexed: false },
-      { name: 'tickUpper', type: 'int24', indexed: false },
+      { name: 'tickLower', type: 'int24', indexed: true },
+      { name: 'tickUpper', type: 'int24', indexed: true },
       { name: 'amount', type: 'uint128', indexed: false },
       { name: 'amount0', type: 'uint256', indexed: false },
       { name: 'amount1', type: 'uint256', indexed: false }
@@ -45,8 +45,8 @@ export const UNISWAP_V3_POOL_ABI = [
     inputs: [
       { name: 'owner', type: 'address', indexed: true },
       { name: 'recipient', type: 'address', indexed: false },
-      { name: 'tickLower', type: 'int24', indexed: false },
-      { name: 'tickUpper', type: 'int24', indexed: false },
+      { name: 'tickLower', type: 'int24', indexed: true },
+      { name: 'tickUpper', type: 'int24', indexed: true },
       { name: 'amount0', type: 'uint128', indexed: false },
       { name: 'amount1', type: 'uint128', indexed: false }
     ]
